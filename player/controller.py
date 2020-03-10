f831978bfe6012161d8d1f8107682b83efdbfd56
Mike <31897545+thetopbob@users.noreply.github.com>
2020-03-10 10:00:59 +0000

    Update controller.py
    updated to reflect controller values and nested new if statements to read up/down left/right

--- player/controller.py
+++ player/controller.py
@@ -1,20 +1,28 @@
 from evdev import InputDevice, categorize, ecodes
 
-gamepad = InputDevice('/dev/input/event3')
+gamepad = InputDevice('/dev/input/event1')
 
 # these codes are for the 8bitdo lite controller
-upBtn = 
-downBtn = 
-rightBtn = 
-leftBtn = 
-aBtn = 304
-bBtn = 305
+# the d-pad codes are derived from a combination of code and value
+# up and down are based on code 1, with up being value 0 and down being 65535
+# left and right are code 0, with left being 0 and right being 65535
+upBtn = 0
+downBtn = 65535
+rightBtn = 65535
+leftBtn = 0
+# the rest of the buttons are a simple numerical readout
+aBtn = 305
+bBtn = 304
 xBtn = 307
-yBtn = 308
-r1Btn = 311
-l1Btn = 310
-startBtn = 
-selectBtn = 
+yBtn = 306
+r1Btn = 309
+r2Btn = 5
+l1Btn = 308
+l2Btn = 2
+startBtn = 311
+selectBtn = 310
+
+print(gamepad)
 
 for event in gamepad.read_loop():
     if event.type == ecodes.EV_KEY:
@@ -27,22 +35,26 @@ for event in gamepad.read_loop():
                 print("A")
             elif event.code == xBtn:
                 print("X")
-
-            elif event.code == upBtn:
-                print("up")
-            elif event.code == downBtn:
-                print("down")
-            elif event.code == leftBtn:
-                print("left")
-            elif event.code == rightBtn:
-                print("right")
-
             elif event.code == startBtn:
                 print("start")
             elif event.code == selectBtn:
                 print("select")
-
             elif event.code == l1Btn:
                 print("left bumper")
             elif event.code == r1Btn:
                 print("right bumper")
+            elif event.code == l2Btn:
+                print("L2 button")
+            elif event.code == r2Btn:
+                print("R2 Button")
+    elif event.type == ecodes.EV_ABS:
+        if event.code == 1:
+            if event.value == upBtn:
+                print("up")
+            elif event.value == downBtn:
+                print("down")
+        elif event.code == 0:
+            if event.value == leftBtn:
+                print("left")
+            elif event.value == rightBtn:
+                print("right")
