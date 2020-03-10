44d262b1065a041be7bfc7061561d4e3091380e4
Mike <31897545+thetopbob@users.noreply.github.com>
2020-03-10 14:27:12 +0000

    Update controller.py

--- player/controller.py
+++ player/controller.py
@@ -1,15 +1,24 @@
 from evdev import InputDevice, categorize, ecodes
+from time import sleep
+from threating import Thread
+import time
+import queue
 
 gamepad = InputDevice('/dev/input/event1')
 
 # these codes are for the 8bitdo lite controller
-# the d-pad codes are derived from a combination of code and value
-# up and down are based on code 1, with up being value 0 and down being 65535
-# left and right are code 0, with left being 0 and right being 65535
-upBtn = 1
-downBtn = 1
-rightBtn = 0
+# the d-pad codes are derived from a combination of code and value:
+# up and down are based on code 1, when up pressed the value starts at 0 (pressed) and on release is 32768
+# down starts as 65535 and reverts to 32768 on release. Lets assume that 32768 becomes the "neutral" value for
+# the Y-axis and build on that.
+# Simlarly for the x-axis, left and right are code 0, with left being 0 
+# and right being 65535
+upBtn = 0
+downBtn = 65535
+yaxisNeutral = 32768
+rightBtn = 65535
 leftBtn = 0
+xaxisNeutral = 32768
 # the rest of the buttons are a simple numerical readout
 aBtn = 305
 bBtn = 304
@@ -22,6 +31,7 @@ selectBtn = 310
 # r2 and l2 are recorded as EV_ABS and on a different value, 1023
 l2Btn = 2
 r2Btn = 5
+currentBtn = 0
 
 if __name__ == "__main__":
     print(f"Start pushing buttons on your {gamepad}")
@@ -44,19 +54,35 @@ if __name__ == "__main__":
                     print("left bumper")
                 elif event.code == r1Btn:
                     print("right bumper")
+        
         elif event.type == ecodes.EV_ABS:
-            if event.value == 1023:
+            if event.code == 1:
+                if event.value == 0:
+                    currentBtn = "up"
+                    while True:
+                        print(f"{currentBtn}")
+                elif event.value == 65535:
+                    currentBtn = "down"
+                    while True:
+                        print(f"{currentBtn}")
+                elif event.value == 32768:
+                    print(f"{currentBtn} released")
+                    currentBtn = 0
+                    
+            elif event.code == 0:
+                if event.value == 0:
+                    while event.value != 32768:
+                        print("left")
+                        if event.value == 32768:
+                            print("left released")
+                elif event.value == 65535:
+                    while event.value !=32768:
+                        print("right")
+                        if event.value == 32768:
+                            print("left released")
+            
+            elif event.value == 1023:
                 if event.code == l2Btn:
                     print("L2 button")
                 elif event.code == r2Btn:
                     print("R2 button")
-            elif event.value == 65535:
-                if event.code == rightBtn:
-                    print("right")
-                elif event.code == downBtn:
-                    print("down")
-            elif event.value == 0:
-                if event.code == leftBtn:
-                    print("left")
-                elif event.code == upBtn:
-                    print("up")
