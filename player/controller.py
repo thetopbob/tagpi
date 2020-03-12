a6792362c423731366a2b05c79f0a87464283199
Mike <31897545+thetopbob@users.noreply.github.com>
2020-03-12 22:02:32 +0000

    Update controller.py

--- player/controller.py
+++ player/controller.py
@@ -1,42 +1,35 @@
 from approxeng.input.selectbinder import ControllerResource
 from time import sleep
-import RPi.GPIO as GPIO
 
-def circlepress():
-	GPIO.setmode(GPIO.BCM)
-	GPIO.setwarnings(False)
-	GPIO.setup(20,GPIO.OUT)
-	GPIO.setup(21,GPIO.OUT)
-	GPIO.setup(26,GPIO.OUT)
-	GPIO.setup(22,GPIO.OUT)
-	print("ALL LEDs on")
-	GPIO.output(20,GPIO.HIGH)
-	GPIO.output(21,GPIO.HIGH)
-	GPIO.output(26,GPIO.HIGH)
-	GPIO.output(22,GPIO.HIGH)
+class controller():
+	def circlepress():
+		currentbutton = "Circle"
+		# Do something!
 
-def squarepress():
-	print("ALL LEDs off")
-	GPIO.output(20,GPIO.LOW)
-	GPIO.output(21,GPIO.LOW)
-	GPIO.output(26,GPIO.LOW)
-	GPIO.output(22,GPIO.LOW)
-
-while True:
-	try:
-		with ControllerResource() as joystick:
-			print('Found a joystick and connected')
-			#print(joystick.controls)
-			while joystick.connected:
-				joystick.check_presses()
-				#if joystick.has_presses:
-					#print(joystick.presses)
-				if joystick.presses.circle:
-					circlepress()
-				elif joystick.presses.square:
-					squarepress()
-				# Joystick disconnected..
-                                #print('Connection to joystick lost')
+	def squarepress():
+		currentbutton = "Square"
+		# Do something!
+	
+	def main():
+		while True:
+			try:
+				with ControllerResource() as joystick:
+					while joystick.connected:
+						joystick.check_presses()
+						#if joystick.has_presses:
+							#print(joystick.presses)
+						if joystick.presses.circle:
+							circlepress()
+						elif joystick.presses.square:
+							squarepress()
+							# Joystick disconnected..
+              #print('Connection to joystick lost')
+				
+if __name__ == "__main__":
+	print('Found a joystick and connected')
+	#print(joystick.controls)
+	main()
+	
 	except IOError:
         # No joystick found, wait for a bit before trying again
 		print('Unable to find any joysticks')
