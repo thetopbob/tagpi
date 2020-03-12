89926ec6f03c8fc3f9aec04756d4ac76e526e216
Mike <31897545+thetopbob@users.noreply.github.com>
2020-03-12 19:54:59 +0000

    Update 8bitdolite.py

--- player/8bitdolite.py
+++ player/8bitdolite.py
@@ -1,10 +1,8 @@
-from approxeng.input import Controller, Button
+from approxeng.input import Controller, Button, BinaryAxis
 
-MY_VENDOR_ID = 1118
-MY_PRODUCT_ID = 
+__all__ = ['BitDoLite']
 
-
-class MyNewController(Controller):
+class BitDoLite(Controller):
     """
     Driver for my new controller type
     """
@@ -18,11 +16,32 @@ class MyNewController(Controller):
         :param float hot_zone:
             Used to set the hot zone for each :class:`approxeng.input.CentredAxis` in the controller.
         """
-        super(MyNewController, self).__init__(vendor_id=MY_VENDOR_ID,
-                                              product_id=MY_PRODUCT_ID,
-                                              controls=[],
-                                              dead_zone=dead_zone,
-                                              hot_zone=hot_zone)
-
+        super(BitDoLite, self).__init__(
+				controls=[
+					Button("B", 304, sname='cross'),
+					Button("A", 305, sname='circle'),
+					Button("Y", 306, sname='square'),
+					Button("X", 307, sname='triangle'),
+					Button("L1", 308, sname='l1'),
+					Button("R1", 309, sname='r1'),
+					Button("Select", 310, sname='select'),
+					Button("Start", 311, sname='start'),
+					Button("L2", 2, sname='r1'),
+					Button("R2", 5, sname='r2')
+					BinaryAxis("D-Pad Vertical", 0, b1name='dup', b2name='ddown'),
+					BinaryAxis("D-Pad Vertical", 1, b1name='dleft', b2name='dright')
+				],
+                dead_zone=dead_zone,
+                hot_zone=hot_zone)
+	
+	@staticmethod
+    def registration_ids():
+        """
+        :return: list of (vendor_id, product_id) for this controller
+		MY_VENDOR_ID = 1118
+		MY_PRODUCT_ID = 736
+        """
+		return[(1118, 736)]
+	
     def __repr__(self):
-        return 'My new controller'
+        return 'BitDoLite'
