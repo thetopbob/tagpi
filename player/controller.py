8817a80d0898f7d1ec18b75c4e3c11b88c5e9e26
thetopbob <barwickm@gmail.com>
2020-05-25 20:28:02 +0100

    put some logging in to the controller code

--- player/controller.py
+++ player/controller.py
@@ -3,6 +3,13 @@ from approxeng.input.selectbinder import ControllerResource
 from time import sleep
 import RPi.GPIO as GPIO
 import threading
+import logging
+
+logging.basicConfig(
+        filename='controller.log',
+        level=logging.DEBUG,
+        format="%(asctime)s:%(levelname)s:%(message)s"
+        )
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
@@ -29,7 +36,7 @@ GPIO.setup(RED, GPIO.OUT)
 GPIO.setup(GREEN, GPIO.OUT)
 GPIO.setup(BLUE, GPIO.OUT)
 GPIO.setup(IR, GPIO.OUT)
-
+logging.debug("GPIO Pins mapped")
 
 forwardLeft = GPIO.PWM(MOTORAFWD, Frequency)
 reverseLeft = GPIO.PWM(MOTORABK, Frequency)
@@ -45,6 +52,7 @@ class RobotStopException(Exception):
         pass
 
 def LED(color,delay):
+        logging.debug("Player pressed {}".format(LED))
         LED_thread=threading.Thread(target=LED_func,args=[color,delay])
         LED_thread.daemon=True
         LED_thread.start()
@@ -96,6 +104,7 @@ while True:
         try:
                 with ControllerResource() as joystick:
                         print('Found a joystick and connected')
+                        logging.debug("Found a joystick and connected")
                         while joystick.connected:
                                 ddown_held, dup_held, dleft_held, dright_held = joystick['ddown','dup','dleft','dright']
                                 if dup_held is not None:
@@ -124,10 +133,12 @@ while True:
         except IOError:
         # No joystick found, wait for a bit before trying again
                 print('Unable to find any joysticks. Trying again in 5 seconds..')
+                logging.error("Cannot find a joystick")
                 sleep(5.0)
 
         except RobotStopException:
                 motor_stop()
+                logging.debug("Player exited the app")
                 print("Exiting...")
                 GPIO.cleanup()
                 sys.exit()
